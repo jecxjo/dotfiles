bc2c3679bddeb18dc2face57453c3a86f2fcbaa3
Jeff Parent <jeff@commentedcode.org>
2024-06-17 22:19:47 -0500

    Really fixed the lsp support this time in lua for nvim

--- nvim/after/plugin/lsp-support.lua
+++ nvim/after/plugin/lsp-support.lua
@@ -1,12 +1,53 @@
-local lsp_zero = require('lsp-zero')
+local vim = vim
+vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
 
-lsp_zero.on_attach(function(client, bufnr)
-    lsp_zero.default_keymaps({ buffer = bufnr })
-end)
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'LSP actions',
+    callback = function(event)
+        local opts = { buffer = event.buf}
+
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', 'g"', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set('n', 'g=', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', 'gx', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    end
+})
+
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local default_setup = function(server)
+    require('lspconfig')[server].setup({
+        capabilities = lsp_capabilities,
+    })
+end
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
+    ensure_installed = {},
     handlers = {
-        lsp_zero.default_setup,
+        default_setup,
+    }
+})
+
+local cmp = require('cmp')
+
+cmp.setup({
+    sources = {
+        {name = 'nvim_lsp'},
     },
+    mapping = cmp.mapping.preset.insert({
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    }),
 })
--- nvim/lua/plugins/vimspector.lua
+++
@@ -1,5 +0,0 @@
-local h = require('helpers')
-
-h.g.vimspector_enable_mappings = 'HUMAN'
-h.map('n', '<leader>di', '<Plug>VimspectorBalloonEval')
-h.map('x', '<leader>di', '<Plug>VimspectorBalloonEval')
